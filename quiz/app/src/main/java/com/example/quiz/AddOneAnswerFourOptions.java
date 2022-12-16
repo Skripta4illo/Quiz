1af7a0f8810fe98babc76cb671cb48f6893226e3
Skripta4illo <109029091+Skripta4illo@users.noreply.github.com>
2022-12-16 10:34:51 +0800

    4 answer question adding
    updeated for all types of questions

--- quiz/app/src/main/java/com/example/quiz/AddOneAnswerFourOptions.java
+++ quiz/app/src/main/java/com/example/quiz/AddOneAnswerFourOptions.java
@@ -33,61 +33,92 @@ public class AddOneAnswerFourOptions extends Fragment {
         binding.buttonFirst.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                boolean fillCorrect = false;
+                singleToneClassAns singleToneClassAns = com.example.quiz.singleToneClassAns.getInstance();
+                String qt = singleToneClassAns.getAns();
+
                 try {
+                    if (qt == "One Answer")
                     if((Integer.parseInt(binding.answer1.getText().toString()) + Integer.parseInt(binding.answer2.getText().toString()) +
                             Integer.parseInt(binding.answer3.getText().toString()) + Integer.parseInt(binding.answer4.getText().toString()) == 1) &&
                             (binding.answer1.getText().toString().equals("0") || binding.answer1.getText().toString().equals("1")) &&
                             (binding.answer2.getText().toString().equals("0") || binding.answer2.getText().toString().equals("1")) &&
                             (binding.answer3.getText().toString().equals("0") || binding.answer3.getText().toString().equals("1")) &&
-                            (binding.answer4.getText().toString().equals("0") || binding.answer4.getText().toString().equals("1"))){
-                        //record to database
-                        DatabaseHandler db = new DatabaseHandler(getContext());
-                        Quiz newQuiz = new Quiz();
-                        newQuiz.setQuizType("One Answer");
-                        newQuiz.setQuizName(binding.quizName.getText().toString());
-                        newQuiz.setAnsCount(4);
-                        db.addQuiz(newQuiz);
-
-                        long nqId = db.getQuizCount();
-
-                        DatabaseQuestionHandler dbq = new DatabaseQuestionHandler(getContext());
-
-                        Question ques1 = new Question();
-                        ques1.setQuizID(nqId);
-                        ques1.setQuestionName(binding.question1.getText().toString());
-                        ques1.setQuestionRight(Integer.parseInt(binding.answer1.getText().toString()));
-                        dbq.addQuestion(ques1);
-
-                        Question ques2 = new Question();
-                        ques2.setQuizID(nqId);
-                        ques2.setQuestionName(binding.question2.getText().toString());
-                        ques2.setQuestionRight(Integer.parseInt(binding.answer2.getText().toString()));
-                        dbq.addQuestion(ques2);
-
-                        Question ques3 = new Question();
-                        ques3.setQuizID(nqId);
-                        ques3.setQuestionName(binding.question3.getText().toString());
-                        ques3.setQuestionRight(Integer.parseInt(binding.answer3.getText().toString()));
-                        dbq.addQuestion(ques3);
-
-                        Question ques4 = new Question();
-                        ques4.setQuizID(nqId);
-                        ques4.setQuestionName(binding.question4.getText().toString());
-                        ques4.setQuestionRight(Integer.parseInt(binding.answer4.getText().toString()));
-                        dbq.addQuestion(ques4);
-
-                        NavHostFragment.findNavController(AddOneAnswerFourOptions.this)
-                                .navigate(R.id.action_AddOneAnswerFourOption_to_SecondFragment);
-                    }
-                    else{
+                            (binding.answer4.getText().toString().equals("0") || binding.answer4.getText().toString().equals("1")))
+                        fillCorrect = true;
+                    else
                         Toast.makeText(getActivity(), "Only one answer should be correct and answers should be only 0 or 1", Toast.LENGTH_SHORT).show();
-                    }
+
+                    if (qt == "Few Answers")
+                        if((binding.answer1.getText().toString().equals("0") || binding.answer1.getText().toString().equals("1")) &&
+                                (binding.answer2.getText().toString().equals("0") || binding.answer2.getText().toString().equals("1")) &&
+                                (binding.answer3.getText().toString().equals("0") || binding.answer3.getText().toString().equals("1")) &&
+                                (binding.answer4.getText().toString().equals("0") || binding.answer4.getText().toString().equals("1")))
+                            fillCorrect = true;
+                        else
+                            Toast.makeText(getActivity(), "Only one answer should be correct and answers should be only 0 or 1", Toast.LENGTH_SHORT).show();
+
+                    if (qt == "Prioritize")
+                        if((Integer.parseInt(binding.answer1.getText().toString()) > 0 && Integer.parseInt(binding.answer1.getText().toString()) <5) &&
+                                (Integer.parseInt(binding.answer2.getText().toString()) > 0 && Integer.parseInt(binding.answer2.getText().toString()) <5) &&
+                                (Integer.parseInt(binding.answer3.getText().toString()) > 0 && Integer.parseInt(binding.answer3.getText().toString()) <5) &&
+                                (Integer.parseInt(binding.answer4.getText().toString()) > 0 && Integer.parseInt(binding.answer4.getText().toString()) <5) &&
+                                (Integer.parseInt(binding.answer1.getText().toString()) != Integer.parseInt(binding.answer2.getText().toString())) &&
+                                (Integer.parseInt(binding.answer1.getText().toString()) != Integer.parseInt(binding.answer3.getText().toString())) &&
+                                (Integer.parseInt(binding.answer1.getText().toString()) != Integer.parseInt(binding.answer4.getText().toString())) &&
+                                (Integer.parseInt(binding.answer3.getText().toString()) != Integer.parseInt(binding.answer2.getText().toString())) &&
+                                (Integer.parseInt(binding.answer4.getText().toString()) != Integer.parseInt(binding.answer2.getText().toString())) &&
+                                (Integer.parseInt(binding.answer4.getText().toString()) != Integer.parseInt(binding.answer3.getText().toString())))
+                            fillCorrect = true;
+                        else
+                            Toast.makeText(getActivity(), "Only one answer should be correct and answers should be between 1 and 4 and should be unique", Toast.LENGTH_SHORT).show();
 
                 }
                 catch (Exception e){
-                    Toast.makeText(getActivity(), "Answer could be only 1 or 0", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getActivity(), "Enter correct answers", Toast.LENGTH_SHORT).show();
                 }
 
+                if (fillCorrect) {
+                    //record to database
+                    DatabaseHandler db = new DatabaseHandler(getContext());
+                    Quiz newQuiz = new Quiz();
+                    newQuiz.setQuizType(qt);
+                    newQuiz.setQuizName(binding.quizName.getText().toString());
+                    newQuiz.setAnsCount(4);
+                    db.addQuiz(newQuiz);
+
+                    long nqId = db.getQuizCount();
+
+                    DatabaseQuestionHandler dbq = new DatabaseQuestionHandler(getContext());
+
+                    Question ques1 = new Question();
+                    ques1.setQuizID(nqId);
+                    ques1.setQuestionName(binding.question1.getText().toString());
+                    ques1.setQuestionRight(Integer.parseInt(binding.answer1.getText().toString()));
+                    dbq.addQuestion(ques1);
+
+                    Question ques2 = new Question();
+                    ques2.setQuizID(nqId);
+                    ques2.setQuestionName(binding.question2.getText().toString());
+                    ques2.setQuestionRight(Integer.parseInt(binding.answer2.getText().toString()));
+                    dbq.addQuestion(ques2);
+
+                    Question ques3 = new Question();
+                    ques3.setQuizID(nqId);
+                    ques3.setQuestionName(binding.question3.getText().toString());
+                    ques3.setQuestionRight(Integer.parseInt(binding.answer3.getText().toString()));
+                    dbq.addQuestion(ques3);
+
+                    Question ques4 = new Question();
+                    ques4.setQuizID(nqId);
+                    ques4.setQuestionName(binding.question4.getText().toString());
+                    ques4.setQuestionRight(Integer.parseInt(binding.answer4.getText().toString()));
+                    dbq.addQuestion(ques4);
+
+                    NavHostFragment.findNavController(AddOneAnswerFourOptions.this)
+                            .navigate(R.id.action_AddOneAnswerFourOption_to_SecondFragment);
+                }
             }
         });
     }
--- quiz/app/src/main/java/com/example/quiz/AddQuestionOptions.java
+++ quiz/app/src/main/java/com/example/quiz/AddQuestionOptions.java
@@ -31,24 +31,34 @@ public class AddQuestionOptions extends Fragment {
         binding.buttonFirst.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //check question type
-                //check if the only answer is true
+                //send question type
+                singleToneClassAns singleToneClassAns = com.example.quiz.singleToneClassAns.getInstance();
+                if (binding.firstAnswer.isChecked())
+                    //singleToneClassAns.setAns(binding.firstAnswer.getText().toString());
+                    singleToneClassAns.setAns("One Answer");
+                if (binding.SecondAnswer.isChecked())
+                    //singleToneClassAns.setAns(binding.SecondAnswer.getText().toString());
+                    singleToneClassAns.setAns("Few Answers");
+                if (binding.ThirdAnswer.isChecked())
+                    //singleToneClassAns.setAns(binding.ThirdAnswer.getText().toString());
+                    singleToneClassAns.setAns("Prioritize");
 
-                if (!binding.ans4.isChecked() && !binding.ans5.isChecked() && !binding.ans6.isChecked() && !binding.ans7.isChecked() && binding.firstAnswer.isChecked())
-                    NavHostFragment.findNavController(AddQuestionOptions.this)
-                        .navigate(R.id.action_AddQuestionOption_to_AddOneAnswerThreeOption);
-                if (binding.ans4.isChecked() && binding.firstAnswer.isChecked())
-                    NavHostFragment.findNavController(AddQuestionOptions.this)
-                            .navigate(R.id.action_AddQuestionOption_to_AddOneAnswerFourOption);
-                if (binding.ans5.isChecked() && binding.firstAnswer.isChecked())
-                    NavHostFragment.findNavController(AddQuestionOptions.this)
-                            .navigate(R.id.action_AddQuestionOption_to_AddOneAnswerFiveOption);
-                if (binding.ans6.isChecked() && binding.firstAnswer.isChecked())
-                    NavHostFragment.findNavController(AddQuestionOptions.this)
-                            .navigate(R.id.action_AddQuestionOption_to_AddOneAnswerSixOption);
-                if (binding.ans7.isChecked() && binding.firstAnswer.isChecked())
-                    NavHostFragment.findNavController(AddQuestionOptions.this)
-                            .navigate(R.id.action_AddQuestionOption_to_AddOneAnswerSevenOption);
+                //check if the only answer is true
+                    if (!binding.ans4.isChecked() && !binding.ans5.isChecked() && !binding.ans6.isChecked() && !binding.ans7.isChecked())
+                        NavHostFragment.findNavController(AddQuestionOptions.this)
+                                .navigate(R.id.action_AddQuestionOption_to_AddOneAnswerThreeOption);
+                    if (binding.ans4.isChecked())
+                        NavHostFragment.findNavController(AddQuestionOptions.this)
+                                .navigate(R.id.action_AddQuestionOption_to_AddOneAnswerFourOption);
+                    if (binding.ans5.isChecked())
+                        NavHostFragment.findNavController(AddQuestionOptions.this)
+                                .navigate(R.id.action_AddQuestionOption_to_AddOneAnswerFiveOption);
+                    if (binding.ans6.isChecked())
+                        NavHostFragment.findNavController(AddQuestionOptions.this)
+                                .navigate(R.id.action_AddQuestionOption_to_AddOneAnswerSixOption);
+                    if (binding.ans7.isChecked())
+                        NavHostFragment.findNavController(AddQuestionOptions.this)
+                                .navigate(R.id.action_AddQuestionOption_to_AddOneAnswerSevenOption);
             }
         });
     }
