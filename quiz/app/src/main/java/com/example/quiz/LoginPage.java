7f1c8be74fda648d12fdc6b28f69632158bf4a9c
Skripta4illo <109029091+Skripta4illo@users.noreply.github.com>
2022-12-19 11:56:20 +0800

    email and password validation

--- quiz/app/src/main/java/com/example/quiz/LoginPage.java
+++ quiz/app/src/main/java/com/example/quiz/LoginPage.java
@@ -3,9 +3,12 @@ package com.example.quiz;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.util.Patterns;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.navigation.fragment.NavHostFragment;
 
@@ -39,6 +42,15 @@ public class LoginPage extends Fragment {
         binding.tvConfPass.setVisibility(View.INVISIBLE);
         binding.confPass.setVisibility(View.INVISIBLE);
         binding.tvRegist.setVisibility(View.INVISIBLE);
+        binding.login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //check if user is exist
+                NavHostFragment.findNavController(LoginPage.this)
+                        .navigate(R.id.action_LoginRegister_to_SecondFragment);
+            }
+        });
+
         binding.tvNewUser.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -48,6 +60,7 @@ public class LoginPage extends Fragment {
                 binding.Name.setVisibility(View.GONE);
                 binding.TVpass.setVisibility(View.GONE);
                 binding.password.setVisibility(View.GONE);
+                binding.login.setVisibility(View.GONE);
                 //show registration fields
                 binding.tvNewName.setVisibility(View.VISIBLE);
                 binding.newName.setVisibility(View.VISIBLE);
@@ -60,7 +73,46 @@ public class LoginPage extends Fragment {
                 binding.tvRegist.setVisibility(View.VISIBLE);
             }
         });
+        binding.tvRegist.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String getEmailId = binding.newEmail.getText().toString();
+
+                // Check if email id is valid or not
+                if (!emailValidator(getEmailId)){
+                    Toast.makeText(LoginPage.this.getContext(), "Your Email Id is Invalid", Toast.LENGTH_SHORT).show();
+                }
+                else
+                    if (binding.newPass.getText().toString().length() < 8){
+                        Toast.makeText(LoginPage.this.getContext(), "Password is too short", Toast.LENGTH_SHORT).show();
+                    }
+                    else
+
+                        //check that the passwords are the same
+                        if (!binding.newPass.getText().toString().equals(binding.confPass.getText().toString())){
+                            Toast.makeText(LoginPage.this.getContext(), "Passwords should be the same!", Toast.LENGTH_SHORT).show();
+                        }
+                        else
+                            Toast.makeText(LoginPage.this.getContext(), "User is created", Toast.LENGTH_SHORT).show();
+                        //adding to the database
+
+            }
+        });
     }
+
+    public boolean emailValidator(String emailToText) {
+
+        // Android offers the inbuilt patterns which the entered
+        // data from the EditText field needs to be compared with
+        // In this case the entered data needs to compared with
+        // the EMAIL_ADDRESS, which is implemented same below
+        if (!emailToText.isEmpty() && Patterns.EMAIL_ADDRESS.matcher(emailToText).matches()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
 
     @Override
     public void onDestroyView() {
